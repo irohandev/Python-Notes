19317b5f2b911bb604a2ea4af2bb53f516ec1459
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-03-05 12:34:41 +0530

    Updated

---
+++ 13_VIRTUAL/codes_for_venv.py
@@ -0,0 +1,6 @@
+# first install the command in terminal = pip isntall virtualenv
+# for creating virtual Environment = python -m venv venv_name
+# for activating virtual Environment = 1) .\venv_name\Scripts\activate {for Windows}
+#                                      2) source/env_name/bin/activate {for Mac & Linux}
+# when you are inside the virtual Environment just install the necessary module which will be required and install using PIP INSTALL command....after installation and working is complete uninstall using PIP UNINSTALL command 
+# for deactivating virtual Environment just type = deactivate
