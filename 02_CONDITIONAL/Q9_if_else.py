136f458f515b118608fc1d11ca12c5fcf402f254
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-05-19 20:15:21 +0530

    Update

--- 02_CONDITIONAL/Q9_if_else.py
+++ 02_CONDITIONAL/Q9_if_else.py
@@ -6,5 +6,6 @@ year = int(input("Enter the Year : "))
 
 if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
     print( year, " is a leap year")
+
 else:
     print(year, "is NOT a leap year")
